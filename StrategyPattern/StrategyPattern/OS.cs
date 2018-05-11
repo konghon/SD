7d68e54168ddb634a9646a2bde17e2e0fac39699
Konghon <konghon@live.nl>
2018-05-11 18:16:53 +0200

    Dump

---
+++ StrategyPattern/StrategyPattern/OS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class OS
+    {
+        private Dictionary<int, int> storageBlocks;
+        private List<DiskSystem> disks;
+        private IScheduler FIFOScheduler;
+        private IScheduler SSTFScheduler;
+        private IScheduler SCANScheduler;
+
+        public OS()
+        {
+
+        }
+
+        public Dictionary<int, int> ReadFromDisk(IScheduler scheduler, int block, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteTodisk(IScheduler scheduler, int block )
+        { }
+    }
+}
---
+++ StrategyPattern/StrategyPattern/ReadWriteOP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    struct ReadWriteOP
+    {
+        public bool RWFlag;
+        public int Block;
+        public int Parameter;
+    }
+}
